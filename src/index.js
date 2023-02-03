1b8bcb1dd6c6556ed95813ce2684df73ed99b252
marsmallowz <alsandymaulana15@email.com>
2023-02-03 09:40:18 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT;
+const routes = require("./routes");
+const { urlencoded } = require("body-parser");
+console.log(PORT);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+app.use(cors());
+
+const db = require("./models");
+db.sequelize.sync();
+
+app.get("/", (_, res) => {
+  res.send("api is running");
+});
+
+app.listen(PORT, () => {
+  console.log("server is running on Port " + PORT);
+});
