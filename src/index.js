f376b14e482a97ff7d3395c69e41da7681748aa2
marsmallowz <alsandymaulana15@email.com>
2023-02-03 10:49:14 +0700

    Change package json and add auth route at index/js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = 2000;
 const routes = require("./routes");
 const { urlencoded } = require("body-parser");
 console.log(PORT);
@@ -16,6 +16,8 @@ app.use(cors());
 
 const db = require("./models");
 db.sequelize.sync();
+
+app.use("/auth", routes.authRoute);
 
 app.get("/", (_, res) => {
   res.send("api is running");
